e158b2e2d62f93a6e84f754eefd22d4b5763c065
KlasL4243 <lukasklassen4243@gmail.com>
2024-05-24 19:51:27 +0200

    chore: create empty flutter project

---
+++ flutter_rote_20_gewinnt/README.md
@@ -0,0 +1,3 @@
+# flutter_rote_20_gewinnt
+
+A new Flutter project.
---
+++ flutter_rote_20_gewinnt/android/app/src/main/kotlin/com/example/flutter_rote_20_gewinnt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_rote_20_gewinnt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
